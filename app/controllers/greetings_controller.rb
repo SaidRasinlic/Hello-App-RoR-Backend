8e3a65e1a43510b786d1b6b41a57dad793ff4dfb
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-10-29 21:51:35 +0200

    Initial commit

--- app/controllers/greetings_controller.rb
+++
@@ -1,5 +0,0 @@
-class GreetingsController < ApplicationController
-  def index
-    render json: Greeting.all.sample
-  end
-end
--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  resources :greetings, only: [:index]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root "greetings#index"
-end
